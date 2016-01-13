7a8c28db2336f6300f945b6fbdf6df829fc85450
Yasmeen Roumie <y4smeen@gmail.com>
2016-01-13 12:48:27 -0500

    idk

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ import urllib2, json, urllib, math, time, string
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods=["POST","GET"])
+@app.route('/index', methods=["POST","GET"])
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
+#    if request.method == 'POST':
+#        start = request.form['start']
+#        end = request.form['end']
+#
+#    else:
+#        return render_template("base.html")
 
 if __name__ == "__main__":
    app.debug = True
