0c1b0bd7c94f8dde0ec1c5fe8d785b31bab3fe67
Yasmeen Roumie <y4smeen@gmail.com>
2016-01-21 11:56:31 -0500

    idk

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ import urllib2, json, urllib, math, time, string
 
 app = Flask(__name__)
 
-@app.route('/', methods=["POST","GET"])
+@app.route('/' ) #methods=["POST","GET"]
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 #    if request.method == 'POST':
 #        start = request.form['start']
 #        end = request.form['end']
-#
+#        startCor = geo_loc()
+#        endCor = geo_loc()
+#        return render_template("results.html")
 #    else:
 #        return render_template("base.html")
 
 if __name__ == "__main__":
    app.debug = True
    app.secret_key = "Don't store this on github"
-   app.run(host="0.0.0.0", port=8000)
+   app.run(host="0.0.0.0", port=7000)
